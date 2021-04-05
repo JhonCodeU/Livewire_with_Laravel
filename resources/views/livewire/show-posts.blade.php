3944f3a9334a92367348f3a4ef7c74fe8c966964
JhonCodeU <jhon.380172793@ucaldas.edu.co>
2021-04-05 13:07:16 -0500

    Creation of Models, migrations and factory and view of livewire

---
+++ resources/views/livewire/show-posts.blade.php
@@ -0,0 +1,6 @@
+<div>
+    
+    <h1>Hello world</h1>
+
+    {{$posts}}
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\ShowPosts;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', ShowPosts::class)->name('dashboard');
+
+Route::get('test/{name}',ShowPosts::class);
