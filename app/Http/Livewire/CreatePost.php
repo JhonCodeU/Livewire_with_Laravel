6aa034c4c98edf2bf7981e9ee542db6320ac0452
JhonCodeU <jhon.380172793@ucaldas.edu.co>
2021-04-12 12:05:15 -0500

    Cagar imagenes con livewire

--- app/Http/Livewire/CreatePost.php
+++ app/Http/Livewire/CreatePost.php
@@ -4,15 +4,19 @@ namespace App\Http\Livewire;
 
 use App\Models\Post;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class CreatePost extends Component
 {
+    use WithFileUploads;
+
     public $open = true;
-    public $title, $content;
+    public $title, $content, $image, $identificator;
 
     protected $rules = [
         'title' => 'required|max:15',
         'content' => 'required',
+        'image' => 'required|image|max:2048',
     ];
 
     public function updated($propertyName)
@@ -25,16 +29,26 @@ class CreatePost extends Component
         return view('livewire.create-post');
     }
 
+    public function mount()
+    {
+        $this->identificator = rand();
+    }
+
     public function save()
     {
         $this->validate();
 
+        $image = $this->image->store('posts');
+
         Post::create([
             'title' => $this->title,
             'content' => $this->content,
+            'image' => $image
         ]);
 
-        $this->reset(['open', 'title', 'content']);
+        $this->reset(['open', 'title', 'content', 'image']);
+
+        $this->identificator = rand();
 
         $this->emitTo('show-posts','render');
         $this->emit('alert', 'The post was  created successfully');
--- resources/views/livewire/create-post.blade.php
+++ resources/views/livewire/create-post.blade.php
@@ -11,6 +11,15 @@
 
         <x-slot name="content">
 
+            <div wire:loading wire:target="image" class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Imagen loading!</strong>
+                <span class="block sm:inline">Wait a moment to imagen is in process.</span>
+            </div>
+
+            @if ($image)
+                <img class="mb-4" src="{{$image->temporaryUrl()}}">
+            @endif
+
             <div class="mb-4">
                 <x-jet-label value="Title of post"/>
                 <x-jet-input type="text" class="w-full" wire:model="title"/>
@@ -21,9 +30,13 @@
             <div class="mb-4">
                 <x-jet-label value="Content of post"/>
                 <textarea class="form-control w-full" rows="6" wire:model="content"></textarea>
+                <x-jet-input-error for="content"/>
             </div>
 
-            <x-jet-input-error for="content"/>
+            <div>
+                <input type="file" wire:model="image" id="{{$identificator}}">
+                <x-jet-input-error for="image"/>
+            </div>
 
         </x-slot>
 
@@ -32,7 +45,7 @@
                 Cancel
             </x-jet-secondary-button>
 
-            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save" class="disabled:opacity-25">
+            <x-jet-danger-button wire:click="save" wire:loading.attr="disabled" wire:target="save, image" class="disabled:opacity-25">
                 Create Post
             </x-jet-danger-button>
 
