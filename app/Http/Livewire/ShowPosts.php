51347665713b2a1ff94e65dd58d7b49682dd5e7e
JhonCodeU <jhon.380172793@ucaldas.edu.co>
2021-04-28 10:55:22 -0500

    add new methods in show post and edit post

--- app/Http/Livewire/ShowPosts.php
+++ app/Http/Livewire/ShowPosts.php
@@ -4,13 +4,26 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
 
 class ShowPosts extends Component
 {
 
-    public $search;
+    public $search, $post, $image, $identificator;
     public $sort = 'id';
     public $direction = 'desc';
+
+    public $open_edit = false;
+
+    public $rules = [
+        'post.title' => 'required',
+        'post.content' => 'required',
+    ];
+
+    public function mount(){
+        $this->identificator = rand();
+        $this->post = new Post();
+    }
 
     protected $listeners = ['render'];
 
@@ -37,5 +50,26 @@ class ShowPosts extends Component
                 $this->direction == 'asc';
             }
         }        
+    }
+
+    public function edit(Post $post){
+        $this->post = $post;
+        $this->open_edit = true;
+    }
+
+    public function update(){
+        $this->validate();
+
+        if ($this->image) {
+            Storage::delete([$this->post->image]);
+            $this->post->image = $this->image->store('posts');
+        }
+
+        $this->post->update();
+
+        $this->reset(['open_edit', 'image']);
+        $this->identificator = rand();
+
+        $this->emit('alert', 'The post was updated successfully');
     }
 }
--- resources/views/livewire/show-posts.blade.php
+++ resources/views/livewire/show-posts.blade.php
@@ -76,24 +76,27 @@
                 </tr>
                 </thead>
                 <tbody class="bg-white divide-y divide-gray-200">
-                @foreach ($posts as $post)
+                @foreach ($posts as $item)
                 <tr>
                     <td class="px-6 py-4">
-                        <div class="text-sm text-gray-900">{{$post->id}}</div>
+                        <div class="text-sm text-gray-900">{{$item->id}}</div>
                     </td>
                     <td class="px-6 py-4">
-                    <div class="text-sm text-gray-900">{{$post->title}}</div>
+                    <div class="text-sm text-gray-900">{{$item->title}}</div>
                     <div class="text-sm text-gray-500">Optimization</div>
                     </td>
                     <td class="px-6 py-4">
-                        <div class="text-sm text-gray-900">{{$post->content}}</div>
+                        <div class="text-sm text-gray-900">{{$item->content}}</div>
                     </td>   
                     <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                        @livewire('edit-post', ['post' => $post], key($post->id))
+                        {{-- @livewire('edit-post', ['post' => $post], key($post->id)) --}}
+                        <a class="btn btn-green" wire:click="edit({{$item}})">
+                            <i class="fas fa-edit"></i>
+                        </a>
                     </td>
                 </tr>
                 @endforeach
-                <!-- More items... -->
+                <!-- More items-->
                 </tbody>
             </table>
             @else
@@ -103,4 +106,51 @@
             @endif
         </x-table>
     </div>
+
+    <x-jet-dialog-modal wire:model="open_edit">
+
+        <x-slot name='title'>
+            {{$post->title}}
+        </x-slot>
+
+        <x-slot name='content'>
+
+            <div wire:loading wire:target="image" class="mb-4 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                <strong class="font-bold">Imagen loading!</strong>
+                <span class="block sm:inline">Wait a moment to imagen is in process.</span>
+            </div>
+
+            @if ($image)
+                <img class="mb-4" src="{{$image->temporaryUrl()}}">
+            @else
+                <img class="mb-4" src="{{Storage::url($post->image)}}">
+            @endif
+
+            <div>
+                <x-jet-label value="Title of the post"/>
+                <x-jet-input wire:model="post.title" type="text" class="w-full" />
+            </div>
+
+            <div>
+                <x-jet-label value="Content of the post"/>
+                <textarea rows="6" wire:model="post.content" class="form-control w-full"></textarea>
+            </div>
+
+            <div>
+                <input type="file" wire:model="image" id="{{$identificator}}">
+                <x-jet-input-error for="image"/>
+            </div>
+        </x-slot>
+
+        <x-slot name='footer'>
+            <x-jet-secondary-button wire:click="$set('open_edit', false)">
+                Cancel
+            </x-jet-secondary-button>
+
+            <x-jet-danger-button wire:click="update" wire:loading.attr="disabled" class="disabled:opacity-25">
+                Update
+            </x-jet-danger-button>
+        </x-slot>
+
+    </x-jet-dialog-modal>
 </div>
